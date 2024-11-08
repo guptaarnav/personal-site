25b97ee89ac0d627bcf0cd99b67f6ab63b72b7ed
arnav <arnav@agupta.org>
2024-11-07 17:03:44 -0800

    fixing prod static image serving

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,6 +26,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'textures', to: 'textures' }, // Copy textures folder to dist/textures
+      ],
     }),
   ],
   devServer: {
